3be88d31979f3b92999ccd25938ac71950225e8b
UNDogMan <52745509+UNDogMan@users.noreply.github.com>
2021-05-07 21:36:54 +0300

    Complete scaffolding  (#3)
    * Complite scaffolding
    
    * Add Aliase for System.Trading.Task

--- DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/UnitOfWork.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/UnitOfWork.cs
@@ -1,6 +1,7 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using DIMS_Core.DataAccessLayer.Interfaces;
 using DIMS_Core.DataAccessLayer.Models;
+using Task = System.Threading.Tasks.Task;
 
 namespace DIMS_Core.DataAccessLayer.Repositories
 {
